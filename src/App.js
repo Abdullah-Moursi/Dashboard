f0fafa0387be72569e29fa2bd2f3e6b86ab6751e
Abdullah-Moursi <moursiabdullah@gmail.com>
2021-10-13 23:41:52 +0200

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import Topbar from './components/topbar/Topbar'
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+import "./app.css";
 function App() {
   return (
     <div>
-      <Topbar/>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
       </div>
+    </div>
   );
 }
 
